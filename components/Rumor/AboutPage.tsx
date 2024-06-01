65595977192c160fd8f9ae292e84275dce7cb3ab
HTML_CSS <vsingh@appcorelabas.com>
2024-06-01 10:15:03 +0530

    hr tags

--- components/Rumor/AboutPage.tsx
+++ components/Rumor/AboutPage.tsx
@@ -23,16 +23,17 @@ const AboutPage: React.FC<AboutPageProps> = ({ blok }) => {
       <div className='flex w-11/12 flex-col items-center'>
         <div className='flex w-full justify-center'>
           <img
-            className='relative  top-[-8rem] max-h-[29rem] w-11/12 object-cover sm:top-[-11rem]  md:-top-[13rem] lg:top-[-15rem] xl:top-[-16rem]'
+            className='relative  top-[-8rem] max-h-[29rem] w-11/12 object-cover sm:top-[-11rem]  md:-top-[14rem] lg:top-[-15rem] xl:top-[-16rem]'
             src={image.filename}
             alt='img'
           />
         </div>
-        <div className=' mt-[-7rem] w-11/12 sm:w-8/12 sm:pt-1 xl:mt-[-13rem]'>
+        <div className=' mt-[-7rem] w-11/12 sm:w-8/12 sm:pt-1 md:mt-[-12rem] xl:mt-[-13rem]'>
           <h1 className='text-center'>{Title}</h1>
           <p className='my-5 text-center text-sm font-medium'>{Description}</p>
           <p className='text-center text-sm font-normal'>{Description_2}</p>
         </div>
+        <hr className=' mt-8 w-4/12 border-2 border-black' />
       </div>
     </div>
   );
--- components/Rumor/Header.tsx
+++ components/Rumor/Header.tsx
@@ -6,48 +6,51 @@ const Header = ({ blok }) => {
   const LastThreeElements = blok.body.slice(3, 6);
 
   return (
-    <div
-      className='flex w-full items-center pt-8 sm:flex-row md:flex-row'
-      {...storyblokEditable(blok)}
-    >
-      <div className='hidden flex-1 flex-wrap justify-around md:flex'>
-        {firstThreeElements.map((item, index) => (
-          <div key={index} className='mx-2 text-white'>
-            <p
-              style={{
-                color: item.FontColor.color,
-                fontSize: `${item.FontSize}rem`,
-                fontWeight: item.FontWeigh,
-              }}
-            >
-              {item.text}
-            </p>
-          </div>
-        ))}
+    <>
+      <div
+        className='flex w-full items-center pt-8 sm:flex-row md:flex-row'
+        {...storyblokEditable(blok)}
+      >
+        <div className='hidden flex-1 flex-wrap justify-around md:flex'>
+          {firstThreeElements.map((item, index) => (
+            <div key={index} className='mx-2 text-white'>
+              <p
+                style={{
+                  color: item.FontColor.color,
+                  fontSize: `${item.FontSize}rem`,
+                  fontWeight: item.FontWeigh,
+                }}
+              >
+                {item.text}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className='mx-4 hidden flex-shrink-0 px-4 md:flex md:px-8 lg:px-12 xl:px-16'>
+          <img
+            src={blok.Image.filename}
+            alt=''
+            className='lg:w-30 xl:w-50 w-10 sm:w-16 md:w-20'
+          />
+        </div>
+        <div className='hidden flex-1 flex-wrap justify-around md:flex '>
+          {LastThreeElements.map((item, index) => (
+            <div key={index} className='mx-2 text-white'>
+              <p
+                style={{
+                  color: item.FontColor.color,
+                  fontSize: `${item.FontSize}rem`,
+                  fontWeight: item.FontWeigh,
+                }}
+              >
+                {item.text}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className='mx-4 hidden flex-shrink-0  px-4 md:flex md:px-8 lg:px-12 xl:px-16'>
-        <img
-          src={blok.Image.filename}
-          alt=''
-          className='lg:w-30 xl:w-50 w-10  sm:w-16 md:w-20'
-        />
-      </div>
-      <div className='hidden flex-1 flex-wrap justify-around md:flex '>
-        {LastThreeElements.map((item, index) => (
-          <div key={index} className='mx-2 text-white'>
-            <p
-              style={{
-                color: item.FontColor.color,
-                fontSize: `${item.FontSize}rem`,
-                fontWeight: item.FontWeigh,
-              }}
-            >
-              {item.text}
-            </p>
-          </div>
-        ))}
-      </div>
-    </div>
+      <hr className='w-full border-[1px] border-white pt-2' />
+    </>
   );
 };
 
